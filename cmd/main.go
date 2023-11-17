c61914df9ea4a9064c577040fb24201a849327f4
diss <65290988+Dissurender@users.noreply.github.com>
2023-11-17 07:05:48 -0600

    init

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"sync"
+)
+
+type Backend struct {
+	URL         *url.URL
+	Alive       bool
+	mux         sync.RWMutex
+	ReverseProx *httputil.ReverseProxy
+}
+
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
+u, _ := url.Parse("http://localhost:8080")
+rp := httputil.NewSingleHostReverseProxy(u)
 
 func main() {
+
+	http.HandlerFunc(rp.ServeHTTP)
+	
 	fmt.Println("Test")
+
+	os.Exit(0)
 }
